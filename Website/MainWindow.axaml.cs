6c916e01b81efe3beeff7045995d205517f4a035
IMalygosI <dmitriy.udaltcov@gmail.com>
2024-04-03 16:43:19 +0300

    Добавьте файлы проекта.

---
+++ Website/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Website
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
